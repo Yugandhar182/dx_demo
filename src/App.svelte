3b8ab4582af2cba7957493d8fc4408732358a41e
Yugandhar182 <yugandhar@recruitly.io>
2023-06-10 22:57:31 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -83,7 +83,7 @@
 				const response = await fetch(
 				  `https://api.recruitly.io/api/candidate/${updatedRowData.id}?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`,
 				  {
-					method: "PUT",
+					method: "POST",
 					headers: {
 					  "Content-Type": "application/json",
 					},
