796c779fa6ef470223c37e00afeb229a279a189d
Yugandhar182 <yugandhar@recruitly.io>
2023-06-10 23:59:36 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,83 @@
 <script>
-	import { onMount } from "svelte";
-	import DevExpress from "devextreme";
-	
+	// Sample data
 	let jsonData = [];
-	
+	let data = [];
+  
 	onMount(async () => {
 	  const response = await fetch(
 		"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
 	  );
 	  const responseData = await response.json();
 	  jsonData = responseData.data;
-	
-	  const gridData = jsonData.map((item) => ({
-		id: item.reference,
-		firstName: item.fullName,
-		surname: item.fullName,
-		email: item.email,
-		mobile: item.mobile,
-	  }));
-	
-	  createDataGrid(gridData);
-	});
-	
-	function createDataGrid(gridData) {
-	  const dataGrid = new DevExpress.ui.dxDataGrid(
-		document.getElementById("dataGrid"),
-		{
-		  dataSource: gridData,
-		  columns: [
-			{ dataField: "id", caption: "ID" },
-			{ dataField: "firstName", caption: "First Name" },
-			{ dataField: "surname", caption: "Surname" },
-			{ dataField: "email", caption: "Email" },
-			{ dataField: "mobile", caption: "Mobile" },
-		  ],
-		  showBorders: true,
-		  filterRow: {
-			visible: true,
-		  },
-		  editing: {
-			allowAdding: false, // Disable adding new rows
-			allowUpdating: false, // Disable updating existing rows
-			allowDeleting: false, // Disable deleting rows
-		  },
-		  paging: {
-			pageSize: 10,
-		  },
-		  pager: {
-			showPageSizeSelector: true,
-			allowedPageSizes: [5, 10, 20],
-			showInfo: true,
-		  },
-		}
-	  );
-	}
-  </script>
+	  console.log(jsonData, "json");
   
-  <div id="dataGrid"></div>
+	  const gridData = jsonData.map((item) => ({
+		reference: item.reference,
+		name: item.fullName,
+		email: item.email,
+		phone: item.mobile,
+	  }));
+  
+	  const dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
+		dataSource: gridData,
+		columns: [
+		  { dataField: "reference", caption: "ID" },
+		  { dataField: "name", caption: "Name", dataType: "url" },
+		  { dataField: "email", caption: "Email" },
+		  { dataField: "phone", caption: "Mobile" },
+		],
+		showBorders: true,
+		filterRow: {
+		  visible: true,
+		},
+		editing: {
+		  allowDeleting: true,
+		  allowAdding: true,
+		  allowUpdating: true,
+		  mode: "popup",
+		  form: {
+			labelLocation: "top",
+		  },
+		  popup: {
+			showTitle: true,
+			title: "Row in the editing state",
+		  },
+		  onRowInserted: function (e) {
+			// Call a function to save the added row to the API
+			saveRowToAPI(e.data);
+		  },
+		},
+		paging: {
+		  pageSize: 10,
+		},
+		pager: {
+		  showPageSizeSelector: true,
+		  allowedPageSizes: [5, 10, 20],
+		  showInfo: true,
+		},
+	  });
+  
+	  function saveRowToAPI(rowData) {
+		fetch("https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154", {
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(rowData),
+		})
+		  .then((response) => {
+			if (response.ok) {
+			  console.log("Row data saved successfully!");
+			  // Refresh the data grid after successful save
+			  dataGrid.refresh();
+			} else {
+			  console.error("Failed to save row data:", response.status);
+			}
+		  })
+		  .catch((error) => {
+			console.error("An error occurred while saving row data:", error);
+		  });
+	  }
+	});
+  </script>
   
