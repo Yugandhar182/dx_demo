a6c31f469f94adff9f58069e61e1c4ae3b6c3200
Yugandhar182 <yugandhar@recruitly.io>
2023-06-11 07:41:06 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -131,7 +131,7 @@
 			console.log("Data being sent to API:", e.data);
 			try {
 			  const response = await fetch(
-				`https://api.recruitly.io/api/candidate/delete?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
+				`https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
 				{
 				  method: "DELETE",
 				  headers: {
