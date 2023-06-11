0bcf2033e6e79f77468af2541857c96f4190830a
Yugandhar182 <yugandhar@recruitly.io>
2023-06-11 05:34:27 +0530

    ,

--- src/App.svelte
+++ src/App.svelte
@@ -1,157 +1,112 @@
 <script>
-	import { onMount } from "svelte";
-	import DevExpress from "devextreme";
-  
-	let jsonData = [];
-	let data = [];
-  
-	onMount(async () => {
-	  const response = await fetch(
-		"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
-	  );
-	  const responseData = await response.json();
-	  jsonData = responseData.data;
-  
-	  const gridData = jsonData.map((item) => ({
-		id: item.reference,
-		firstName: item.fullName,
-		surname: item.fullName,
-		email: item.email,
-		mobile: item.mobile,
-	  }));
-  
-	  createDataGrid(gridData);
-	});
-  
-	function createDataGrid(gridData) {
-	  const dataGrid = new DevExpress.ui.dxDataGrid(
-		document.getElementById("dataGrid"),
-		{
-		  dataSource: gridData,
-		  columns: [
-			{ dataField: "id", caption: "ID" },
-			{ dataField: "firstName", caption: "First Name" },
-			{ dataField: "surname", caption: "Surname" },
-			{ dataField: "email", caption: "Email" },
-			{ dataField: "mobile", caption: "Mobile" },
-		  ],
-		  showBorders: true,
-		  filterRow: {
-			visible: true,
-		  },
-		  editing: {
-			allowDeleting: true,
-			allowAdding: true,
-			allowUpdating: true,
-			mode: "popup",
-			form: {
-			  labelLocation: "top",
-			},
-			popup: {
-			  showTitle: true,
-			  title: "Row in the editing state",
-			},
-			onRowInserted: async (e) => {
-			  const newRowData = e.data;
-			
-			  try {
-				const response = await fetch(
-				  "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154",
-				  {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json",
-					},
-					body: JSON.stringify(newRowData),
-				  }
-				);
-  
-				
-				if (response.ok) {
-					return fetch();
-				  // Handle success
-				  console.log("New row added successfully");
-				  
-				} else {
-				  // Handle error
-				  console.error("Failed to add new row");
-				}
-			  } catch (error) {
-				// Handle error
-				console.error("Failed to add new row", error);
-			  }
-			},
-			onRowUpdated: async (e) => {
-			  const updatedRowData = e.data;
-			 
-			  try {
-				const response = await fetch(
-				  `https://api.recruitly.io/api/candidate/${updatedRowData.id}?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
-				  {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json",
-					},
-					body: JSON.stringify(updatedRowData),
-				  }
-				);
-  
-				
-  
-				if (response.ok) {
-					return fetch();
-				  // Handle success
-				  console.log("Row updated successfully");
-				  
-				} else {
-				  // Handle error
-				  console.error("Failed to update row");
-				}
-			  } catch (error) {
-				// Handle error
-				console.error("Failed to update row", error);
-			  }
-			},
-			onRowRemoved: async (e) => {
-			  const removedRowData = e.data;
-			  
-			  try {
-				const response = await fetch(
-				  `https://api.recruitly.io/api/candidate/${removedRowData.id}?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154`,
-				  {
-					method: "DELETE",
-				  }
-				);
-				console.log(removedRowData);
-				
-            
-				if (response.ok) {
-					return fetch();
-				  // Handle success
-				  console.log("Row deleted successfully");
-				  
-				} else {
-				  // Handle error
-				  console.error("Failed to delete row");
-				}
-			  } catch (error) {
-				// Handle error
-				console.error("Failed to delete row", error);
-			  }
-			},
-		  },
-		  paging: {
-			pageSize: 30,
-		  },
-		  pager: {
-			showPageSizeSelector: true,
-			allowedPageSizes: [5, 10, 20 , 30],
-			showInfo: true,
-		  },
-		}
-	  );
-	}
-  </script>
-  
-  <div id="dataGrid"></div>
-  
+  import { onMount } from "svelte";
+  import DevExpress from "devextreme";
+
+  let jsonData = [];
+  let gridData = [];
+
+  onMount(async () => {
+    const response = await fetch(
+      "https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E"
+    );
+    const responseData = await response.json();
+    jsonData = responseData.data;
+    console.log(jsonData, "json");
+
+    gridData = jsonData.map((item) => ({
+      id: item.id,
+      firstname: item.firstname,
+      surname: item.surname,
+      email: item.email,
+      mobile: item.mobile,
+    }));
+
+    console.log(gridData, "griddata");
+
+    const dataGrid = new DevExpress.ui.dxDataGrid(
+      document.getElementById("dataGrid"),
+      {
+        dataSource: gridData,
+        columns: [
+          { dataField: "id", caption: "ID" },
+          { dataField: "firstname", caption: "First Name" },
+          { dataField: "surname", caption: "Surname" },
+          { dataField: "email", caption: "Email" },
+          { dataField: "mobile", caption: "Mobile" },
+        ],
+        showBorders: true,
+        filterRow: {
+          visible: true,
+        },
+        editing: {
+          allowDeleting: true,
+          allowAdding: true,
+          allowUpdating: true,
+          mode: "popup",
+          form: {
+            labelLocation: "top",
+          },
+          popup: {
+            showTitle: true,
+            title: "Row in the editing state",
+          },
+          onRowInserted: async (e) => {
+            const newRowData = e.data;
+            try {
+              const response = await fetch(
+                "https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(newRowData),
+                }
+              );
+
+              if (response.ok) {
+                // Handle success
+                console.log("New row added successfully");
+
+                // Fetch the updated data from the API
+                const updatedResponse = await fetch(
+                  "https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E"
+                );
+                const updatedData = await updatedResponse.json();
+
+                // Update the jsonData and gridData variables with the new data
+                jsonData = updatedData.data;
+                gridData = jsonData.map((item) => ({
+                  id: item.id,
+                  firstname: item.firstname,
+                  surname: item.surname,
+                  email: item.email,
+                  mobile: item.mobile,
+                }));
+
+                // Assign the updated data to the grid dataSource
+                dataGrid.option("dataSource", gridData);
+              } else {
+                // Handle error
+                console.error("Failed to add new row");
+              }
+            } catch (error) {
+              // Handle error
+              console.error("Failed to add new row", error);
+            }
+          },
+        },
+        paging: {
+          pageSize: 10,
+        },
+        pager: {
+          showPageSizeSelector: true,
+          allowedPageSizes: [5, 10, 20],
+          showInfo: true,
+        },
+      }
+    );
+  });
+</script>
+
+<div id="dataGrid"></div>
